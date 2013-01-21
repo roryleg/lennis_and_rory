c78177766cbf9b74d11358d5933c6cdaeff5330d
Rory Joyce <your.rory.d.joyce@gmail.com>
2013-01-20 18:38:56 -0800

    Initial app commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+  def venue
+  end
+  def where_to_stay
+  end
+  def travel
+  end
+  def local_activities
+  end
+  def pics
+  end
+  def registry
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LennisAndRory::Application.config.secret_token = 'e8a00ace329cf8f38430b97fc534c9c3d095068516fae47e4eb516f9ed95fed41e280f957fcccfed0586eddc06a0ff00a011a260ec2d0e09df3678e8cbd53976'
